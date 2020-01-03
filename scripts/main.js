f62ab1c4d9ec01d547f40dd1b639baee22e5520a
Zach Barbre <zach.barbre@gmail.com>
2020-01-03 15:16:39 -0500

    Add index.html main.js styles.css and images

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+let myImage = document.querySelector('img');
+let timer = setTimeout(getCthulhu, 20000);
+
+function getCthulhu() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/me.png') {
+        myImage.setAttribute('src', 'images/cthulhu.jpg');
+        timer = setTimeout(getCthulhu, 250);
+    }
+    else {
+        myImage.setAttribute('src', 'images/me.png');
+        timer = setTimeout(getCthulhu, 20000);
+    }
+}
